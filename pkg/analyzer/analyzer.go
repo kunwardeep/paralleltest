950e9ba72fca1e7a2dadd71fde3d3349db6cf5ce
kunwardeep <kunwardeep@gmail.com>
2020-09-25 12:49:17 +1000

    Run only for test methods"

--- pkg/analyzer/analyzer.go
+++ pkg/analyzer/analyzer.go
@@ -1,13 +1,11 @@
 package analyzer
 
 import (
+	"fmt"
 	"go/ast"
-	"strings"
-
+	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
-
-	"golang.org/x/tools/go/analysis"
 )
 
 var Analyzer = &analysis.Analyzer{
@@ -19,6 +17,7 @@ var Analyzer = &analysis.Analyzer{
 
 func run(pass *analysis.Pass) (interface{}, error) {
 	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
 	nodeFilter := []ast.Node{
 		(*ast.FuncDecl)(nil),
 	}
@@ -30,39 +29,42 @@ func run(pass *analysis.Pass) (interface{}, error) {
 		rangeHasParallelMethod := false
 
 		// TODO : Only run for test files
-		if strings.HasPrefix(funcDecl.Name.Name, "Test_") {
-			for _, l := range funcDecl.Body.List {
-				switch v := l.(type) {
+		if !isItATestFunction(funcDecl) {
+			return
+		}
 
-				case *ast.ExprStmt:
-					ast.Inspect(v, func(n ast.Node) bool {
-						if funcHasParallelMethod == false {
-							funcHasParallelMethod = callExprCallsMethodParallel(n)
-						}
-						return true
-					})
+		for _, l := range funcDecl.Body.List {
+			switch v := l.(type) {
 
-				case *ast.RangeStmt:
-					// TODO: Check range statements is over testcases
+			case *ast.ExprStmt:
+				ast.Inspect(v, func(n ast.Node) bool {
+					if funcHasParallelMethod == false {
+						funcHasParallelMethod = callExprCallsMethodParallel(n)
+					}
+					return true
+				})
 
-					rangeStatementExists= true
-					// TODO: Also check for the assignment tc:tc
-					ast.Inspect(v, func(n ast.Node) bool {
-						if rangeHasParallelMethod == false {
-							rangeHasParallelMethod = callExprCallsMethodParallel(n)
-						}
-						return true
-					})
-				}
-			}
+			case *ast.RangeStmt:
+				// TODO: Check range statements is over testcases and not any other ranges
 
-			if !funcHasParallelMethod {
-				pass.Reportf(node.Pos(), "Function %s missing the call to method parallel \n", funcDecl.Name.Name)
-			}
-			if rangeStatementExists && !rangeHasParallelMethod {
-				pass.Reportf(node.Pos(), "Range statement %s missing the call to method parallel \n", funcDecl.Name.Name)
+				rangeStatementExists = true
+				// TODO: Also check for the assignment tc:tc
+				ast.Inspect(v, func(n ast.Node) bool {
+					if rangeHasParallelMethod == false {
+						rangeHasParallelMethod = callExprCallsMethodParallel(n)
+					}
+					return true
+				})
 			}
 		}
+
+		if !funcHasParallelMethod {
+			pass.Reportf(node.Pos(), "Function %s missing the call to method parallel \n", funcDecl.Name.Name)
+		}
+		if rangeStatementExists && !rangeHasParallelMethod {
+			pass.Reportf(node.Pos(), "Range statement for test %s missing the call to method parallel \n", funcDecl.Name.Name)
+		}
+
 	})
 
 	return nil, nil
@@ -76,6 +78,30 @@ func callExprCallsMethodParallel(node ast.Node) bool {
 	case *ast.CallExpr:
 		if fun, ok := n.Fun.(*ast.SelectorExpr); ok {
 			return fun.Sel.Name == methodName
+		}
+	}
+	return false
+}
+
+func isItATestFunction(funcDecl *ast.FuncDecl) bool {
+	testMethodParamName := "t"
+	testMethodPackageType := "testing"
+	testMethodStruct := "T"
+
+	if funcDecl.Type.Params != nil {
+		if len(funcDecl.Type.Params.List) > 1 {
+			return false
+		}
+
+		for _, param := range funcDecl.Type.Params.List {
+			if param.Names[0].String() == testMethodParamName {
+				if starExp, ok := param.Type.(*ast.StarExpr); ok {
+					if selectExpr, ok := starExp.X.(*ast.SelectorExpr); ok {
+						return fmt.Sprint(selectExpr.X) == testMethodPackageType &&
+							fmt.Sprint(selectExpr.Sel) == testMethodStruct
+					}
+				}
+			}
 		}
 	}
 	return false
